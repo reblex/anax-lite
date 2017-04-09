efa7803a619269bffe0d001966e6418c307c38be
Simon Wahlström <simon.otdw@gmail.com>
2017-04-09 18:16:24 +0200

    Add Kmom01 Report and fix link colors

--- view/take1/report.php
+++ view/take1/report.php
@@ -16,9 +16,42 @@
                 </div>
             </div>
             <div class="text">
-                Här kommer det.
+                <b><i>Hur känns det att hoppa rakt in i klasser med PHP, gick det bra?</i></b>
+                <br>
+                Det var roligt och jag tyckte att det gick mycket bra. Jag har
+                såklart en del tidigare erfarenhet med klasser och objekt nu
+                vilket gjorde att det var lätt att hoppa in i samma struktur i
+                PHP. Något jag märker är hur jag blir bättre och bättre på att
+                strukturera min kod i allmänhet, och nu specifikt i klasser.
+                Ibland kan det bli lite rörigt men jag tycker ändå att det går
+                frammåt med fart!
+                <br><br>
+                <b><i>Berätta om dina reflektioner kring ramverk, anax-lite och din me-sida.</i></b>
+                <br>
+                Första gången vi jobbade med ett ramverk, alltså i Designkursen,
+                tyckte jag att det var jobbigt. Man hade ingen aning om hur större
+                delen av koden fungerade och man satt bara och nya saker här och
+                där. Nu däremot kan jag hänga med helt på hur allt fungerar, vilket
+                är vad jag vill, genom att vara med när hela ramverket byggs upp.
+                Jag tycker att det har gått mycket bra, även fast det var ett väldigt
+                stort första kursmoment. Dock förstår jag varför, det känns som man
+                nu har en väldigt stabil grund att bygga på.
+                <br><br>
+                Det finns såklart fortfarande en del som jag inte är helt införstådd
+                i och kan tycka vara knepiga. Alla filer som finns i default/views
+                tycker jag kan vara svåra att tyda och förstå hur man ska använda.
+                Dock är ju detta som jag nämnde tidigare det förstakursmomentet
+                och det finns mycket tid kvar att lära sig.
+                <br><br>
+                <b><i>Gick det bra att komma igång med MySQL, har du liknande erfarenheter sedan tidigare?</i></b>
+                <br>
+                Jag har jobbat med SQL en hel del tidigare och tycker att det är
+                ganska enkelt. Det är en enkel struktur och språket är inte direkt
+                svårt att minnas. Därför hade jag heller inte några problem med att
+                komma igång med början av SQL-uppgiften.
             </div>
         </div>
 
     </div>
+    <br><br>
 </main>
